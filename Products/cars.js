4283ec424388891475ef9465407be97a052b96ed
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-07-13 16:21:51 -0700

    products added

--- Products/cars.js
+++ Products/cars.js
@@ -1,9 +1,49 @@
-const sportsCar = {
-    id: 'carLot-sports',
-    name: 'LAMBORGHINI',
+export default CARS;
+
+const lamborghini = {
+    id: 'lamborghini',
+    name: 'GALLARDO',
     image: '../assets/sportscar.png',
     category: 'sports-car',
     price: 50.00,
-    description: 'Red, 2 Seater mid-engine sports car'
+    description: 'Red, 2 Seater mid-engine sports car',
 
 };
+
+const mercedes = {
+    id: 'mercedes',
+    name: 'G-Wagon',
+    image: '../assets/luxurysuv.png',
+    category: 'luxury-suv',
+    price:55.00,
+    description: 'Black, luxury 5 passenger suv',
+
+}
+
+const bmw = {
+    id: 'bmw',
+    name: 'x5',
+    image: '../assets/bmwsuv.jpg',
+    category: 'suv',
+    price: 30.00,
+    description: 'Maroon, 5 passenger suv',
+}
+
+const vw = {
+    id: 'vw',
+    name: 'VW Bus',
+    image: '../assets/vwvan.png',
+    category: 'van',
+    price: 35.00,
+    description: 'Orange, 7 passenger van'
+}
+
+const porsche = {
+    id: 'porsche',
+    name: 'Porsche 911',
+    image: '../assets/porsche.png',
+    category: 'race-car',
+    price: 60.00,
+    description: 'Black, 1 seater racing car'
+}
+
