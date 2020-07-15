fb729ebe9ef40e9cc5c1289a25efca6308a2a458
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-07-14 21:00:04 -0700

    added cart data

---
+++ data/cart.js
@@ -0,0 +1,17 @@
+const cart = [{
+    id:'lamborghini'
+    quantity: 3
+}, {
+    id:'vw'
+    quantity:1
+}, {
+    id:'porsche'
+    quantity: 6
+}, {
+    id:'mercedes'
+    quantity: 10
+}];
+
+
+export default cart;
+
